0e2e11bd4b0fc256f6335c9a9610881192ad38d2
Ulrich Johan Isachsen <ulrich@isachsen.org>
2021-08-14 07:19:23 +0200

    TEMP COMMIT: SC4 selection shape

---
+++ RegionMaker/src/application/SC4SelectionShape.java
@@ -0,0 +1,29 @@
+package application;
+
+import javafx.scene.image.Image;
+
+public class SC4SelectionShape extends SelectionShape
+{
+	public SC4SelectionShape(Map map,double imageResX,double imageResY,Image config)
+	{
+		
+	}
+	
+	public SC4SelectionShape(Map map,double imageResX,double imageResY)
+	{
+		
+	}
+	
+	@Override
+	public double getWidth()
+	{
+		return 0;
+	}
+
+	@Override
+	public double getHeight()
+	{
+		return 0;
+	}
+
+}
